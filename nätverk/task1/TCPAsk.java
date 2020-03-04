47f8be92d73f41c41362d47bb6d085e3615212a3
nikitah <nikitah@kth.se>
2020-03-04 11:02:38 +0100

    tcp client

---
+++ nätverk/task1/TCPAsk.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+import tcpclient.TCPClient;
+
+ public class TCPAsk {
+    public static void main( String[] args) {
+        String hostname;
+        int port;
+        String serverInput = null;
+        String serverOutput;
+
+        try {
+            hostname = args[0];
+            port = Integer.parseInt(args[1]);
+            if (args.length >= 3) {
+                // Collect remaining arguments into a single string
+                StringBuilder builder = new StringBuilder();
+                boolean first = true;
+                for (int i = 2; i < args.length; i++) {
+                    if (first)
+                        first = false;
+                    else
+                        builder.append(" ");
+                    builder.append(args[i]);
+                }
+                serverInput = builder.toString();
+            }
+        }
+        catch (Exception ex) {
+            System.err.println("Usage: TCPAsk host port <data to server>");
+            return;
+        }
+        try {
+            if(serverInput != null){
+              serverOutput = TCPClient.askServer(hostname, port, serverInput);
+            }else{
+              serverOutput = TCPClient.askServer(hostname, port);
+            }
+            System.out.printf("%s:%d says:\n%s", hostname, port, serverOutput);
+        } catch(IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
