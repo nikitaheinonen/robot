0ed222849f53a5d12182dcf17a3057e8ab38c5ae
nikitaheinonen <nikita.heinonen@gmail.com>
2022-06-21 15:47:59 +0200

    Add files via upload
    added comments

--- kodtest21/src/main/java/com/advice/RestControllerAdvice.java
+++ kodtest21/src/main/java/com/advice/RestControllerAdvice.java
@@ -9,25 +9,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Advice class for handling exceptions frmo controller
+ */
 @ControllerAdvice
 public class RestControllerAdvice {
 
-
+    /**
+     * Exception for when database is not connected for some reason
+     * @param e exception
+     * @return response entity
+     */
     @ExceptionHandler(value = {DatabaseConnectionException.class})
     public ResponseEntity<String> databaseConnectionException(DatabaseConnectionException e){
         return new ResponseEntity<String>("Could not connect to database", HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Exeption for when the title is empty
+     * @param e exception
+     * @return resposne entity with http code
+     */
     @ExceptionHandler(value = {WrongTitleException.class})
     public ResponseEntity<String> wrongTitleException(WrongTitleException e){
         return new ResponseEntity<String>("Could not find a movie with this title", HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * exception for when no movie was found
+     * @param e exception
+     * @return response entity with http code
+     */
     @ExceptionHandler(value = {NoMovieFoundException.class})
     public ResponseEntity<String> movieNotFoundException(NoMovieFoundException e){
         return new ResponseEntity<String>("Could not find a movie related to this search", HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Exception for when the search is empty
+     * @param e exception
+     * @return response entity with http code
+     */
     @ExceptionHandler(value = {EmptySearchException.class})
     public ResponseEntity<String> emptySearchException(EmptySearchException e){
         return new ResponseEntity<String>("No data entered in search", HttpStatus.BAD_REQUEST);
